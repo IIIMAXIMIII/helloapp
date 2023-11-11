4e7383e1075209c02db95ab08f8e0dee9c0e8e82
black <black@example.com>
2023-11-12 00:04:26 +0500

    Add hello everyone

--- HelloApp/HelloApp/Program.cs
+++ HelloApp/HelloApp/Program.cs
@@ -4,11 +4,6 @@ static class HelloApp
 {
     static void Main()
     {
-        Console.WriteLine("hello");
-    }
-
-    private static void GreetWhite()
-    {
-        Console.WriteLine("hello white");
+        Console.WriteLine("hello everyone");
     }
 }
